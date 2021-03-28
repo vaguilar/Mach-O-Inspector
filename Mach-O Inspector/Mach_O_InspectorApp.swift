97f3bcd314fe65d1f13fe25a7283e0d2bf86b526
Victor Aguilar <victoraguilar90@gmail.com>
2021-03-28 14:16:10 -0700

    add file>open menu

--- Mach-O Inspector/Mach_O_InspectorApp.swift
+++ Mach-O Inspector/Mach_O_InspectorApp.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 class MachOInspectorEnvironment: ObservableObject {
     @Published var machoList: [MachO] = []
+    @Published var title: String = ""
 
     init() {
-        let url = Bundle.main.executableURL!
+        if let url = Bundle.main.executableURL {
+            self.open(url)
+        }
+    }
+
+    func open(_ url: URL) {
         do {
             let fatMacho = try FatMachO.fromURL(url)
-            self._machoList = Published(initialValue: fatMacho.machoList)
+            self.machoList = fatMacho.machoList
+            self.title = url.lastPathComponent
         } catch {
             print(error)
         }
@@ -24,12 +31,30 @@ class MachOInspectorEnvironment: ObservableObject {
 @main
 struct Mach_O_InspectorApp: App {
     @StateObject var env = MachOInspectorEnvironment()
+    @State private var isFilePickerShown = false
+
+    let openPanel = NSOpenPanel()
 
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 ContentView()
                     .environmentObject(env)
+            }
+            .navigationTitle(self.env.title)
+        }
+        .commands {
+            CommandGroup(after: CommandGroupPlacement.newItem) {
+                Button(action: {
+                    openPanel.begin { (result) in
+                        if result == .OK, let url = openPanel.url {
+                            self.env.open(url)
+                        }
+                    }
+                }) {
+                    Text("Open File...")
+                }
+                .keyboardShortcut("o", modifiers: .command)
             }
         }
     }
